043ff5406723c0304cfa8831210750a4f553ae4f
Ali Naeimi <ali.naeimi@outlook.com>
2023-05-30 10:30:07 +0330

    Update page.js

--- app/page.js
+++ app/page.js
@@ -15,6 +15,7 @@ export default function Home() {
             target="_blank"
             rel="noopener noreferrer"
           >
+              heeeeeeeeeeeeey
             By{' '}
             <Image
               src="/vercel.svg"
